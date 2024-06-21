85f6394a5890ba7c23ffdb46598511c8834965fc
Loice Khoene <82638055+Loicemary@users.noreply.github.com>
2024-06-21 13:53:33 +0300

    Add files via upload

---
+++ Admin DashBoard/order.js
@@ -0,0 +1,21 @@
+const Orders =[
+    {
+        productName: 'Web Design',
+        productNumber: '47089',
+        paymentStatus: 'Due',
+        status: 'Pending'
+    },
+    {
+        productName: 'User Experience',
+        productNumber: '93845',
+        paymentStatus: 'Refunded',
+        status: 'Declined'
+    },
+    {
+        productName: 'Motion Design',
+        productNumber: '11240',
+        paymentStatus: 'Paid',
+        status: 'Active'
+    },
+
+]
